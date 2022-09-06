c56fc48104411fa9ebf84e2f7feb266ea53ddf0c
marcoshamil76 <marcoshamilton1976@gmail.com>
2022-09-06 16:15:33 -0300

    Entrega Exercicio 03

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,31 +1,33 @@
-let idade = prompt("Digite sua idade")
-let ensinoMedio2 = Boolean
 
-let ensinoMedio = prompt("Ensino médio concluido? Digite sim ou não")
+
+let idade = prompt("Digite sua idade")
+let ensinoMedio2
+
+let checkSimNao = prompt("Ensino médio concluido? Digite sim ou não")
 
 let isMaiorIdade;
 let isCursandoOutraFaculdade;
-console.log (ensinoMedio)
-if(ensinoMedio === "sim"){
-    ensinoMedio2 = true
-}else{
-    ensinoMedio2 = false
+
+    if(checkSimNao === "sim"){
+        ensinoMedio2 = true
+    }else{
+        ensinoMedio2 = false
 	}
-console.log (ensinoMedio)
-ensinoMedio = prompt("Esta cursando uma faculadade? Digite Sim ou não")
-console.log (ensinoMedio)
-if(ensinoMedio === "sim"){
-    isCursandoOutraFaculdade = true
-}else{
-    isCursandoOutraFaculdade = false
+console.log (checkSimNao)
+checkSimNao = prompt("Esta cursando uma faculadade? Digite Sim ou não")
+console.log (checkSimNao)
+    if(checkSimNao === "sim"){
+        isCursandoOutraFaculdade = true
+    }else{
+        isCursandoOutraFaculdade = false
 	}
 
-if (ensinoMedio2 && isCursandoOutraFaculdade){
-    alert ("Parabéns vc concluiu o ensino médio e já está na Faculdade")
+    if (ensinoMedio2 && isCursandoOutraFaculdade){
+        alert ("Parabéns vc concluiu o ensino médio e já está na Faculdade")
 
-}else if (!isCursandoOutraFaculdade && ensinoMedio2){
-    alert ("Parabéns vc concluiu o ensino médio, e agora o próximo passo é entrar na Faculdade")
-}else{
-    alert ("Continue estudando que logo irá concluir o ensino médio \n e entrar na Faculdade")
+    }else if (!isCursandoOutraFaculdade && ensinoMedio2){
+        alert ("Parabéns vc concluiu o ensino médio, e agora o próximo passo é entrar na Faculdade")
+    }else{
+        alert ("Continue estudando que logo irá concluir o ensino médio \n e entrar na Faculdade")
 }
-    
+   // ensinomedio = confirm("voce terminou o ensino o medio") 
